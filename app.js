58f6f9f9147e4f1baf689a5e2630ce5084418c25
mstanka <stmmarketa@gmail.com>
2021-10-24 10:23:13 +0200

    get req using params

--- app.js
+++ app.js
@@ -19,6 +19,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params); // object that assign value to our parameter
+
+  const id = req.params.id * 1; // to convert from string to number
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tours) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
