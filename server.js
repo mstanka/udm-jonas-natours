09b8ad325fbf3845964e821f10d721333303fa5e
mstanka <stmmarketa@gmail.com>
2021-10-28 17:22:48 +0200

    add mongoose and create first schema

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config/.env' }); // will read our variables from the file and save them in node.js env variables
 // console.log(process.env);
+
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connected');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const app = require('./app');
 
