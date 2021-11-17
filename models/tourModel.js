382bc819f72b25af2f74a4c76ee47dbea90ec349
mstanka <stmmarketa@gmail.com>
2021-11-17 14:56:36 +0100

    document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -66,6 +68,24 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// document middleware: runs before .save() and .create() - to manipulate currently saved documents
+tourSchema.pre('save', function (next) {
+  // 'this' is pointing to the currently saving document
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// pre save hook (middleware
+// tourSchema.pre('save', (next) => {
+//   console.log('Will save the document...');
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
