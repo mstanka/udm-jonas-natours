1e0f8e24b85e4a22676e3257badf927a8448ede6
mstanka <stmmarketa@gmail.com>
2021-11-18 15:10:49 +0100

    errors during dev vs prod, invaid db ids

--- controllers/errorControler.js
+++ controllers/errorControler.js
@@ -1,3 +1,38 @@
+const AppErrError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppErrError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknow error: dont leak error details
+  } else {
+    // 1) log error
+    console.error('ERROR 💥', err);
+    // 2) send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 // 4 parameters - express automatically recognize that this is error handling middleware
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
@@ -6,8 +41,12 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
